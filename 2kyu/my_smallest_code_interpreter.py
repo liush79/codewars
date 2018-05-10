33c6fa9d8ce4132f4a8f197330bd1764cc54356a
seungho.ryu <98koom@hanmail.net>
2018-05-10 15:53:58 +0900

    Added 2kyu/my_smallest_code_interpreter.py (brain luck)

--- 2kyu/my_smallest_code_interpreter.py
+++ 2kyu/my_smallest_code_interpreter.py
@@ -1,6 +1,3 @@
-ptrs = [0] * 100
-
-
 def _find_pair(code):
     cnt = 0
     for i in range(0, len(code)):
@@ -12,46 +9,42 @@ def _find_pair(code):
             return i
 
 
-def process(code, input, idx):
+def process(code, input, ptrs, idx):
     res = ''
     i = 0
-    while True:
-        try:
-            # if i >= len(code) or code[i] in (chr(255), chr(0)):
-            if i >= len(code):
+    while i < len(code):
+        if code[i] == ',':
+            input_pop = ord(input.pop(0))
+            if input_pop in (255, 0):
                 break
-            elif code[i] == ',':
-                input_pop = ord(input.pop(0))
-                if input_pop in (255, 0):
-                    break
-                ptrs[idx] = input_pop
-            elif code[i] == '[' and i != 0:
-                e = _find_pair(code[i:])
-                sub, idx = process(code[i:i+e+1], input, idx)
+            ptrs[idx] = input_pop
+        elif code[i] == '[' and i != 0:
+            e = _find_pair(code[i:])
+            if ptrs[idx] > 0:
+                sub, idx = process(code[i:i+e+1], input, ptrs, idx)
                 res += sub
-                i = i + e
-            elif code[i] == ']':
-                i = 0
-                if ptrs[idx] <= 0:
-                    break
-            elif code[i] == '.':
-                res += chr(ptrs[idx])
-            elif code[i] == '+':
-                ptrs[idx] += 1
-            elif code[i] == '-':
-                ptrs[idx] -= 1
-            elif code[i] == '>':
-                idx += 1
-            elif code[i] == '<':
-                idx -= 1
-            i += 1
-        except Exception, e:
-            break
+            i = i + e
+        elif code[i] == ']':
+            i = 0
+            if ptrs[idx] == 0:
+                break
+        elif code[i] == '.':
+            res += chr(ptrs[idx])
+        elif code[i] == '+':
+            ptrs[idx] = ptrs[idx] + 1 if ptrs[idx] < 255 else 0
+        elif code[i] == '-':
+            ptrs[idx] = ptrs[idx] - 1 if ptrs[idx] > 0 else 255
+        elif code[i] == '>':
+            idx += 1
+        elif code[i] == '<':
+            idx -= 1
+        i += 1
     return res, idx
 
 
 def brain_luck(code, input):
-    return process(code, list(input), 0)[0]
+    ptrs = [0] * 100
+    return process(code, list(input), ptrs, 0)[0]
 
 
 # # Echo until byte(255) encountered
@@ -63,7 +56,7 @@ def brain_luck(code, input):
 # print brain_luck('++++++++++'
 #                  '[>+++++++>++++++++++>+++>+<<<<-]'
 #                  '>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.', '')
-print brain_luck('>,>+++[-<-[.>[-]<+.>]<.>]', '1')
+# print brain_luck('>,>+++[-<-[.>[-]<+.>]<.>]', '1')
 # Two numbers multiplier
 # print brain_luck(',>,<[>[->+>+<<]>>[-<<+>>]<<<-]>>.', chr(8) + chr(9))    # chr(72)
 # times table
