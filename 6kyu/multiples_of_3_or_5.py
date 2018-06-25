581cd7cfb81eab21dae0278bbc5c11a16869aa39
seungho.ryu <98koom@hanmail.net>
2018-06-26 01:32:54 +0900

    Removed unnecessary codes

--- 6kyu/multiples_of_3_or_5.py
+++ 6kyu/multiples_of_3_or_5.py
@@ -1,5 +1,5 @@
 def solution(ns):
-    return sum(set(n for n in range(1, ns) if n % 3 == 0 or n % 5 == 0))
+    return sum(n for n in range(1, ns) if n % 3 == 0 or n % 5 == 0)
 
 
 print solution(10)  # 23
